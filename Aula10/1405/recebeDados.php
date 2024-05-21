43c54f168e3a528ac3b18496386c3187d509ffad
Nayu-mi <129801800+Nayu-mi@users.noreply.github.com>
2024-05-21 09:01:11 -0300

    Add files via upload

---
+++ Aula10/1405/recebeDados.php
@@ -0,0 +1,69 @@
+<?php
+/*
+CREATE DATABASE bancoEmpresa
+USE bancoEmpresa
+CREATE TABLE clientes
+
+Nome            varchar(80)     not null
+Nasc            DATE            NOT NULL
+Endereco        varchar(100)    NOT NULL
+Genero          CHAR            NOT NULL
+novosProdutos   VARCHAR(3)      NOT NULL
+ofertas         VARCHAR(3)      NOT NULL
+*/
+
+//importano porgramfga de conexao.phph
+include_once "conexao.php"; /*pode ser usado tmbv só o iclude, 
+mas o include onde impede que seja utuilizado novamente*/
+
+try{
+
+    $nome = $_POST['fNome'];
+    $nasc = $_POST['fNascimento'];
+    $endereco = $_POST['fEndereco'];
+    $genero = $_POST['fGenero'];
+    $novosProdutos = $_POST["fInfo1"];;
+    $ofertas = $_POST["fInfo2"];
+    
+    $nome = strtoupper($nome);
+    
+    if(empty($_POST["fInfo1"])){
+        $novosProdutos = "nao";
+    }else{
+        $novosProdutos=$_POST["fInfo1"];
+    }
+    
+    if(empty($_POST["fInfo2"])){
+        $ofertas = "nao";
+    }else{
+        $ofertas=$_POST["fInfo2"];
+    }
+
+    /*
+    echo "<h1 style='text align:center; color:violet;'>Estamos no PHP</h1>";
+    echo "<p> Nome digitado: " .$nome. "</p>";
+    echo "<br>";
+    echo "Nome $nome";
+    echo "<br></br>";
+    echo "<p>Data de Nascimento: " .$nasc. "</p>";
+    echo "<br></br>";
+    echo "Gênero: $genero";
+    echo "<br>";
+    echo "Novos produtos: $novosProdutos";
+    echo "<br>";
+    echo "Ofertas: $ofertas";
+    */
+
+    $sql=$conectar->prepare("USE bancoempresa; INSERT INTO pdo.clientes (nome, nascimento, endereco, genero, novosprodutos, ofertas)  
+    values ('$nome', '$nasc', '$endereco', '$genero', '$novosProdutos', '$ofertas')");
+  
+   $sql->execute();
+   header("location: index.html");
+
+}
+catch (PDOException $e) {
+    echo("Falha na gravação do registro! lascou-se foi tudo!!!!".$e->getMessage());
+}
+
+
+?>
